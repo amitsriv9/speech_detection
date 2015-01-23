71db6e657a9da9dde098ee6c70a1fce8f3797fa0
amitsriv9 <amitsriv9@gmail.com>
2015-01-22 21:19:32 -0500

    first commit

---
+++ sound_capture.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <alsa/asoundlib.h>
+
+
+
+  int main(){
+
+  snd_pcm_t *handle;
+  snd_pcm_hw_params_t *hw_params;
+  snd_pcm_uframes_t frames;
+
+  snd_pcm_open(&handle, );
+  
+  return  0;
+  }
+
